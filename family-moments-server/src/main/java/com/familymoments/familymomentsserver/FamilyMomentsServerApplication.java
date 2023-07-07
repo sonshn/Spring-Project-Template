66d1d76b1e540fcf9176db550797f8e9f5550bfd
sonshn <sonshn@naver.com>
2023-07-07 18:55:05 +0900

    feat: exclude datasource 옵션 추가

--- family-moments-server/src/main/java/com/familymoments/familymomentsserver/FamilyMomentsServerApplication.java
+++ family-moments-server/src/main/java/com/familymoments/familymomentsserver/FamilyMomentsServerApplication.java
@@ -2,8 +2,11 @@ package com.familymoments.familymomentsserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// 처음에는 아무런 DataSource가 없기 때문에 exclude 옵션을 꼭 추가
+// DB 연결 후, (exclude = DataSourceAutoConfiguration.class) 삭제
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class FamilyMomentsServerApplication {
 
 	public static void main(String[] args) {
