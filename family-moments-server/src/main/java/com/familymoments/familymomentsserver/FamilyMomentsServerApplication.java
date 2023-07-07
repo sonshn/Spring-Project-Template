20a5a7944b13994bb9aabdcd680a1d654bf02d7d
sonshn <sonshn@naver.com>
2023-07-07 15:34:11 +0900

    feat: add initial project template

---
+++ family-moments-server/src/main/java/com/familymoments/familymomentsserver/FamilyMomentsServerApplication.java
@@ -0,0 +1,13 @@
+package com.familymoments.familymomentsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyMomentsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyMomentsServerApplication.class, args);
+	}
+
+}
---
+++ family-moments-server/src/test/java/com/familymoments/familymomentsserver/FamilyMomentsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familymoments.familymomentsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyMomentsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
